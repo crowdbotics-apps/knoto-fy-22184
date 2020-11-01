6c8c34b56b8e3a7b5a52e0c812fb6865b1eab165
Crowdbotics <team@crowdbotics.com>
2020-11-01 14:53:55 +0000

    Initial commit.

---
+++ ios/knoto_fy_22184Tests/knoto_fy_22184Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface knoto_fy_22184 : XCTestCase
+
+@end
+
+@implementation knoto_fy_22184
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
